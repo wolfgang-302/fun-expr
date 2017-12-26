478c2af135b868fcffd88c89d3d3ad5518e95747
wolfgang-302 <11839527+wolfgang-302@users.noreply.github.com>
2017-12-27 00:39:16 +0100

    corrected type  Changes to be committed: 	modified:   fun_expr/__init__.py

--- fun_expr/__init__.py
+++ fun_expr/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from function_from_expression import (
+from .function_from_expression import (
         Function_from_Expression,
         Named_Function_from_Expression,
         )
